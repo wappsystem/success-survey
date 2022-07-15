33337487e6ba365e1f525cd3ada65e89d3bfe238
Gunnar Unger <gunnar.unger@gmail.com>
2022-07-15 11:02:46 +1000

    update

--- modules/module-list.js
+++ modules/module-list.js
@@ -7,6 +7,7 @@
     var api="wapp";
     //-------------------------------------------------------------------------------------
     m[p+"not_elig1"]         ={url:H+"/form-grid/screening/not_elig1.html"}
+    m[p+"thank_you"]         ={url:H+"/form-grid/screening/thank_you.html"}
     m[p+"pre-screening-data"]   ={url:H+"/form-grid/screening/eligibility-data.html",Table:"succeed-survey-record",router:1 };
     //m[p+"not_eligphq9"]         ={url:H+"/form-grid/screening/not_eligphq9.html"},
    // m[p+"not_elig2"]         ={url:H+"/form-grid/screening/not_elig2.html"},
